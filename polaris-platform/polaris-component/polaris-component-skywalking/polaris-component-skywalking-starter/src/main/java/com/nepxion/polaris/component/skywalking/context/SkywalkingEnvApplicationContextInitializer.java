536948efb492c4c675616132cc7bfd57f9420321
HaojunRen <1394997@qq.com>
2020-08-10 13:57:39 +0800

    初始化代码

---
+++ polaris-platform/polaris-component/polaris-component-skywalking/polaris-component-skywalking-starter/src/main/java/com/nepxion/polaris/component/skywalking/context/SkywalkingEnvApplicationContextInitializer.java
@@ -0,0 +1,36 @@
+package com.nepxion.polaris.component.skywalking.context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.Ordered;
+
+import com.nepxion.polaris.component.common.constant.PolarisConstant;
+import com.nepxion.polaris.component.common.exception.PolarisException;
+import com.nepxion.polaris.component.env.processor.PolarisEnvProcessor;
+
+public class SkywalkingEnvApplicationContextInitializer extends PolarisEnvProcessor implements ApplicationContextInitializer<ConfigurableApplicationContext>, Ordered {
+    private static final Logger LOG = LoggerFactory.getLogger(SkywalkingEnvApplicationContextInitializer.class);
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        if (!(applicationContext instanceof AnnotationConfigApplicationContext)) {
+            try {
+                LOG.info("Initialize Skywalking env...");
+
+                process(PolarisConstant.SKYWALKING_NAME);
+            } catch (Exception e) {
+                LOG.error("Initialize Skywalking env failed", e);
+
+                throw new PolarisException(e);
+            }
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
