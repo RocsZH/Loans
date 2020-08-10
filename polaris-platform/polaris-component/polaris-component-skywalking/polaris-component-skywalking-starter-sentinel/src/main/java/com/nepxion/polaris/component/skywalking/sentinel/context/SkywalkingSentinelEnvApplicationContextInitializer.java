49f9f2f007b70f8d770b916b7b7518dfeddf5abc
HaojunRen <1394997@qq.com>
2020-08-10 14:02:02 +0800

    重构类结构

---
+++ polaris-platform/polaris-component/polaris-component-skywalking/polaris-component-skywalking-starter-sentinel/src/main/java/com/nepxion/polaris/component/skywalking/sentinel/context/SkywalkingSentinelEnvApplicationContextInitializer.java
@@ -0,0 +1,36 @@
+package com.nepxion.polaris.component.skywalking.sentinel.context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.Ordered;
+
+import com.nepxion.polaris.component.common.constant.PolarisConstant;
+import com.nepxion.polaris.component.common.exception.PolarisException;
+import com.nepxion.polaris.component.env.processor.PolarisEnvProcessor;
+
+public class SkywalkingSentinelEnvApplicationContextInitializer extends PolarisEnvProcessor implements ApplicationContextInitializer<ConfigurableApplicationContext>, Ordered {
+    private static final Logger LOG = LoggerFactory.getLogger(SkywalkingSentinelEnvApplicationContextInitializer.class);
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        if (!(applicationContext instanceof AnnotationConfigApplicationContext)) {
+            try {
+                LOG.info("Initialize skywalking-sentinel env...");
+
+                process(PolarisConstant.SKYWALKING_SENTINEL_NAME);
+            } catch (Exception e) {
+                LOG.error("Initialize skywalking-sentinel env failed", e);
+
+                throw new PolarisException(e);
+            }
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
