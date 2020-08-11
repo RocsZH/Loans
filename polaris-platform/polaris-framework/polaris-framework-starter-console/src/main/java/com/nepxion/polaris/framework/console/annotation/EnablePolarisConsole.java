39df67a71faedaca0a81da30c8df14c682dacbcc
HaojunRen <1394997@qq.com>
2020-08-11 11:34:35 +0800

    初始化代码

---
+++ polaris-platform/polaris-framework/polaris-framework-starter-console/src/main/java/com/nepxion/polaris/framework/console/annotation/EnablePolarisConsole.java
@@ -0,0 +1,24 @@
+package com.nepxion.polaris.framework.console.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableApolloConfig
+public @interface EnablePolarisConsole {
+
+}
---
+++ polaris-platform/polaris-framework/polaris-framework-starter-console/src/main/java/com/nepxion/polaris/framework/console/context/PolarisConsoleEnvProcessor.java
@@ -0,0 +1,33 @@
+package com.nepxion.polaris.framework.console.context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import com.nepxion.polaris.component.common.constant.PolarisConstant;
+import com.nepxion.polaris.component.common.exception.PolarisException;
+import com.nepxion.polaris.component.env.processor.PolarisEnvProcessor;
+
+public class PolarisConsoleEnvProcessor extends PolarisEnvProcessor implements EnvironmentPostProcessor {
+    private static final Logger LOG = LoggerFactory.getLogger(PolarisConsoleEnvProcessor.class);
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        try {
+            LOG.info("Initialize {} env...", getName());
+
+            process(environment);
+        } catch (Exception e) {
+            LOG.error("Initialize {} env failed", getName(), e);
+
+            throw new PolarisException(e);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return PolarisConstant.CONSOLE_NAME;
+    }
+}
