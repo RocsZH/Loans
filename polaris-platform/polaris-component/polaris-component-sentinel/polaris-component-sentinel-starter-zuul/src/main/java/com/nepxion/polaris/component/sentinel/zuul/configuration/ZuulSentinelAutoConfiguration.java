a9e088139921a6178d7042d8b07ab0de2157d1e0
HaojunRen <1394997@qq.com>
2020-08-11 13:09:11 +0800

    重构类结构

---
+++ polaris-platform/polaris-component/polaris-component-sentinel/polaris-component-sentinel-starter-zuul/src/main/java/com/nepxion/polaris/component/sentinel/zuul/configuration/ZuulSentinelAutoConfiguration.java
@@ -0,0 +1,30 @@
+package com.nepxion.polaris.component.sentinel.zuul.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulErrorFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPostFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPreFilter;
+
+@Configuration
+public class ZuulSentinelAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    public SentinelZuulPreFilter sentinelZuulPreFilter() {
+        return new SentinelZuulPreFilter();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SentinelZuulPostFilter sentinelZuulPostFilter() {
+        return new SentinelZuulPostFilter();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SentinelZuulErrorFilter sentinelZuulErrorFilter() {
+        return new SentinelZuulErrorFilter();
+    }
+}
