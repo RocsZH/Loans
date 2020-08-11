a385549869378a0e6f885cd922907693e74bf384
HaojunRen <1394997@qq.com>
2020-08-11 14:09:11 +0800

    初始化代码

---
+++ polaris-platform/polaris-component/polaris-component-gray/polaris-component-gray-starter-console/src/main/java/com/nepxion/polaris/component/gray/console/context/GrayConsoleEnvApplicationContextInitializer.java
@@ -0,0 +1,35 @@
+package com.nepxion.polaris.component.gray.console.context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nepxion.polaris.component.common.constant.PolarisConstant;
+import com.nepxion.polaris.component.common.exception.PolarisException;
+import com.nepxion.polaris.component.env.processor.PolarisEnvProcessor;
+
+public class GrayConsoleEnvApplicationContextInitializer extends PolarisEnvProcessor implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    private static final Logger LOG = LoggerFactory.getLogger(GrayConsoleEnvApplicationContextInitializer.class);
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        if (!(applicationContext instanceof AnnotationConfigApplicationContext)) {
+            try {
+                LOG.info("Initialize {} env...", getName());
+
+                process(applicationContext.getEnvironment());
+            } catch (Exception e) {
+                LOG.error("Initialize {} env failed", getName(), e);
+
+                throw new PolarisException(e);
+            }
+        }
+    }
+
+    @Override
+    public String getName() {
+        return PolarisConstant.GRAY_CONSOLE_NAME;
+    }
+}
