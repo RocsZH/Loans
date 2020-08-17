cab25568bedf9e60ae5b4ff19f9efd59690aa10b
HaojunRen <1394997@qq.com>
2020-08-17 22:27:50 +0800

    修改测试用例

--- polaris-platform/polaris-component/polaris-component-env/src/test/java/com/nepxion/polaris/component/env/test/PolarisEnvTest.java
+++ polaris-platform/polaris-component/polaris-component-env/src/test/java/com/nepxion/polaris/component/env/test/PolarisEnvTest.java
@@ -47,9 +47,13 @@ public class PolarisEnvTest {
     @Test
     public void test1() {
         // System.setProperty("test.a", "A");
-        // System.setProperty("test.b", "B");
         // System.setProperty("test.b", "${test.a}-B");
-        // System.setProperty("test.c", "${test.a}-C");
+        // System.setProperty("test.c", "${test.b}-C");
+        // System.setProperty("test.d", "${test.c}-D");
+
+        // 占位符测试。如果System.setProperty已设置，忽略内置默认的配置项，占位符内外可以合并
+        Map<String, Object> propertySource = new HashMap<String, Object>();
+        environment.getPropertySources().addLast(new MapPropertySource(PolarisConstant.POLARIS_PROPERTY_SOURCE, propertySource));
 
         String path = PolarisConstant.META_INF_PATH + "test" + "-" + PolarisConstant.COMMON + "." + PolarisConstant.PROPERTIES_FORMAT;
 
@@ -68,31 +72,27 @@ public class PolarisEnvTest {
         }
 
         for (String key : properties.stringPropertyNames()) {
-            // 如果已经设置，则尊重已经设置的值
             if (environment.getProperty(key) == null && System.getProperty(key) == null && System.getenv(key.toUpperCase()) == null) {
                 String value = properties.getProperty(key);
 
-                LOG.info("* Env parameter : {} = {}", key, value);
-
                 System.setProperty(key, value);
-            } else {
-                LOG.info("* Env parameter : {} has been set", key);
             }
         }
 
-        // 占位符测试。如果System.setProperty已设置，忽略内置默认的配置项，占位符内外可以合并
-        // test.a=a
-        // test.b=${test.a}-b
-        // test.c=${test.b}-c
-        Map<String, Object> polarisPropertySource = new HashMap<String, Object>();
-        environment.getPropertySources().addLast(new MapPropertySource("polarisPropertySource", polarisPropertySource));
-
         for (String key : properties.stringPropertyNames()) {
             String value = properties.getProperty(key);
-            if (StringUtils.isNotBlank(value) && StringUtils.contains(value, "${") && StringUtils.contains(value, "}") && StringUtils.indexOf(value, "}") - StringUtils.indexOf(value, "${") >= 1) {
-                polarisPropertySource.put(key, value);
-                // 占位符转换后，塞回System.setProperty，保证environment.getProperty和System.getProperty都能取到该值
-                System.setProperty(key, environment.getProperty(key));
+            if (environment.getProperty(key) == null && System.getProperty(key) == null && System.getenv(key.toUpperCase()) == null) {
+                if (StringUtils.isNotBlank(value) && StringUtils.contains(value, "${") && StringUtils.contains(value, "}") && StringUtils.indexOf(value, "}") - StringUtils.indexOf(value, "${") >= 1) {
+                    propertySource.put(key, value);
+
+                    // 占位符转换后，塞回System.setProperty，保证environment.getProperty和System.getProperty都能取到该值
+                    String newValue = environment.getProperty(key);
+                    System.setProperty(key, newValue);
+
+                    LOG.info("* Env parameter : {} = {}", key, newValue);
+                }
+            } else {
+                LOG.info("* Env parameter : {} = {}", key, value);
             }
         }
 
@@ -110,9 +110,9 @@ public class PolarisEnvTest {
     // @Test
     public void test2() {
         // System.setProperty("test.a", "A");
-        // System.setProperty("test.b", "B");
         // System.setProperty("test.b", "${test.a}-B");
-        // System.setProperty("test.c", "${test.a}-C");
+        // System.setProperty("test.c", "${test.b}-C");
+        // System.setProperty("test.d", "${test.c}-D");
 
         String path = PolarisConstant.META_INF_PATH + "test" + "-" + PolarisConstant.COMMON + "." + PolarisConstant.PROPERTIES_FORMAT;
 
@@ -131,9 +131,6 @@ public class PolarisEnvTest {
         }
 
         // 占位符测试。如果System.setProperty已设置，忽略内置默认的配置项，占位符内外可以合并
-        // test.a=a
-        // test.b=${test.a}-b
-        // test.c=${test.b}-c
         MutablePropertySources propertySources = new MutablePropertySources();
         propertySources.addFirst(new PropertiesPropertySource("systemProperties", System.getProperties()));
         propertySources.addLast(new PropertiesPropertySource("polarisProperties", properties));
@@ -143,5 +140,11 @@ public class PolarisEnvTest {
         System.out.println(propertyResolver.getProperty("test.a"));
         System.out.println(propertyResolver.getProperty("test.b"));
         System.out.println(propertyResolver.getProperty("test.c"));
+        System.out.println(propertyResolver.getProperty("test.d"));
+
+        System.out.println(System.getProperty("test.a"));
+        System.out.println(System.getProperty("test.b"));
+        System.out.println(System.getProperty("test.c"));
+        System.out.println(System.getProperty("test.d"));
     }
 }
