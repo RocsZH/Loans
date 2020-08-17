82d6b108c4e6c2166b6799aea0bed821c0e22918
HaojunRen <1394997@qq.com>
2020-08-17 18:42:37 +0800

    修改测试用例

--- polaris-platform/polaris-component/polaris-component-env/src/test/java/com/nepxion/polaris/component/env/test/PolarisEnvTest.java
+++ polaris-platform/polaris-component/polaris-component-env/src/test/java/com/nepxion/polaris/component/env/test/PolarisEnvTest.java
@@ -1,7 +1,10 @@
 package com.nepxion.polaris.component.env.test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.ConfigurablePropertyResolver;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySourcesPropertyResolver;
@@ -41,9 +45,70 @@ public class PolarisEnvTest {
     }
 
     @Test
-    public void test() {
-        System.out.println(environment.getProperty("spring.application.name"));
+    public void test1() {
+        // System.setProperty("test.a", "A");
+        // System.setProperty("test.b", "B");
+        // System.setProperty("test.b", "${test.a}-B");
+        // System.setProperty("test.c", "${test.a}-C");
 
+        String path = PolarisConstant.META_INF_PATH + "test" + "-" + PolarisConstant.COMMON + "." + PolarisConstant.PROPERTIES_FORMAT;
+
+        PolarisEnvProcessor polarisEnvProcessor = new PolarisEnvProcessor() {
+            @Override
+            public String getName() {
+                return null;
+            }
+        };
+
+        Properties properties = null;
+        try {
+            properties = polarisEnvProcessor.processProperties(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (String key : properties.stringPropertyNames()) {
+            // 如果已经设置，则尊重已经设置的值
+            if (environment.getProperty(key) == null && System.getProperty(key) == null && System.getenv(key.toUpperCase()) == null) {
+                String value = properties.getProperty(key);
+
+                LOG.info("* Env parameter : {} = {}", key, value);
+
+                System.setProperty(key, value);
+            } else {
+                LOG.info("* Env parameter : {} has been set", key);
+            }
+        }
+
+        // 占位符测试。如果System.setProperty已设置，忽略内置默认的配置项，占位符内外可以合并
+        // test.a=a
+        // test.b=${test.a}-b
+        // test.c=${test.b}-c
+        Map<String, Object> polarisPropertySource = new HashMap<String, Object>();
+        environment.getPropertySources().addLast(new MapPropertySource("polarisPropertySource", polarisPropertySource));
+
+        for (String key : properties.stringPropertyNames()) {
+            String value = properties.getProperty(key);
+            if (StringUtils.isNotBlank(value) && StringUtils.contains(value, "${") && StringUtils.contains(value, "}") && StringUtils.indexOf(value, "}") - StringUtils.indexOf(value, "${") >= 1) {
+                polarisPropertySource.put(key, value);
+                // 占位符转换后，塞回System.setProperty，保证environment.getProperty和System.getProperty都能取到该值
+                System.setProperty(key, environment.getProperty(key));
+            }
+        }
+
+        System.out.println(environment.getProperty("test.a"));
+        System.out.println(environment.getProperty("test.b"));
+        System.out.println(environment.getProperty("test.c"));
+        System.out.println(environment.getProperty("test.d"));
+
+        System.out.println(System.getProperty("test.a"));
+        System.out.println(System.getProperty("test.b"));
+        System.out.println(System.getProperty("test.c"));
+        System.out.println(System.getProperty("test.d"));
+    }
+
+    // @Test
+    public void test2() {
         // System.setProperty("test.a", "A");
         // System.setProperty("test.b", "B");
         // System.setProperty("test.b", "${test.a}-B");
