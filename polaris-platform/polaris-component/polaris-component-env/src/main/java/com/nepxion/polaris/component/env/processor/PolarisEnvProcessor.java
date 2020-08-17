f6a08acb98c8fee6ed6253ef0185ccf6d6f63b6c
HaojunRen <1394997@qq.com>
2020-08-17 17:03:42 +0800

    重构域名中区域的占位格式

--- polaris-platform/polaris-component/polaris-component-env/src/main/java/com/nepxion/polaris/component/env/processor/PolarisEnvProcessor.java
+++ polaris-platform/polaris-component/polaris-component-env/src/main/java/com/nepxion/polaris/component/env/processor/PolarisEnvProcessor.java
@@ -85,25 +85,25 @@ public abstract class PolarisEnvProcessor {
         return processDomainPlaceholder(value, zone);
     }
 
-    // 域名占位处理。占位格式为{-%zone%}
+    // 域名占位处理。占位格式为[-%zone%]
     // 1. 根据server.properties里配置的env和zone，动态解析和创建多活或者多云的域名
-    // 2. 域名表达式，样例：nacos-fat{-%zone%}.nepxion.com，该域名格式为组件-环境-区域.根域，也可以用其它符号代替"-"
-    // 3. 区域，zone表示用来区别多活或者多云的域名后缀或者前缀
+    // 2. 域名表达式，样例：nacos-fat[-%zone%].nepxion.com，该域名格式为组件-环境-区域.根域，也可以用其它符号代替"-"
+    // 3. 区域，zone表示用来区别多活、多云和SET单元化的域名后缀或者前缀
     @SuppressWarnings("deprecation")
     protected String processDomainPlaceholder(String domainExpression, String zone) {
         String zoneExpression = "%" + PolarisConstant.ZONE + "%";
 
         String domain = null;
         // 不符合域名表达式的配置项，不做处理直接返回
-        if (StringUtils.contains(domainExpression, zoneExpression) && StringUtils.contains(domainExpression, "{") && StringUtils.contains(domainExpression, "}") && StringUtils.indexOf(domainExpression, "}") - StringUtils.indexOf(domainExpression, "{") >= zoneExpression.length()) {
+        if (StringUtils.contains(domainExpression, zoneExpression) && StringUtils.contains(domainExpression, "[") && StringUtils.contains(domainExpression, "]") && StringUtils.indexOf(domainExpression, "]") - StringUtils.indexOf(domainExpression, "[") >= zoneExpression.length()) {
             if (StringUtils.isNotBlank(zone)) {
                 // 兼容低版本的commons-langs
                 domain = StringUtils.replaceAll(domainExpression, zoneExpression, zone);
-                domain = StringUtils.replace(domain, "{", StringUtils.EMPTY);
-                domain = StringUtils.replace(domain, "}", StringUtils.EMPTY);
+                domain = StringUtils.replace(domain, "[", StringUtils.EMPTY);
+                domain = StringUtils.replace(domain, "]", StringUtils.EMPTY);
             } else {
                 // 兼容低版本的commons-langs
-                domain = StringUtils.replaceAll(domainExpression, "\\{\\S+\\}", StringUtils.EMPTY);
+                domain = StringUtils.replaceAll(domainExpression, "\\[\\S+\\]", StringUtils.EMPTY);
             }
         } else {
             domain = domainExpression;
