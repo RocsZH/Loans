1eb68d3b86ab12312fd0dcecadbb4fe575321472
HaojunRen <1394997@qq.com>
2020-08-29 10:30:45 +0800

    重构根域和区域逻辑

--- polaris-platform/polaris-component/polaris-component-env/src/main/java/com/nepxion/polaris/component/env/constant/PolarisEnvConstant.java
+++ polaris-platform/polaris-component/polaris-component-env/src/main/java/com/nepxion/polaris/component/env/constant/PolarisEnvConstant.java
@@ -9,19 +9,19 @@ public class PolarisEnvConstant {
     public static final String DOMAIN_VALUE = "nepxion.com";
 
     // 区域名相关定义。包含三种传值方式，优先级至上而下
-    // 1. 通过-Dregion=sha或者System.setProperty("region", "sha")方式进行传入
-    // 2. 通过大写的REGION，其值为sha的System ENV方式进行传入
-    // 3. 通过DevOps在server.properties定义region=sha方式进行传入
+    // 1. 通过-Dregion=sh-pd或者System.setProperty("region", "sh-pd")方式进行传入
+    // 2. 通过大写的REGION，其值为sh-pd的System ENV方式进行传入
+    // 3. 通过DevOps在server.properties定义region=sh-pd方式进行传入
 
     // 区域名分隔符相关定义
     // REGION_SEPARATE表示区域在域名中的分隔符
     // REGION_SEPARATE_PREFIX表示区域在域名中的分隔符是否在前面还是后面
     // 包含两种表现形式。特别注意：region占位符前后切记不要出现分隔符，因为框架会自动去适配
     // 1. 例如，原始格式为nacos-fat${region}.${domain}
-    //    1.1 在region存在的情况下，会解析成nacos-fat-sha.nepxion.com
+    //    1.1 在region存在的情况下，会解析成nacos-fat-sh-pd.nepxion.com
     //    1.2 在region缺失的情况下，会解析成nacos-fat.nepxion.com
     // 2. 例如，原始格式为${region}fat-nacos.${domain}
-    //    2.1 在region存在的情况下，会解析成sha-fat-nacos.nepxion.com
+    //    2.1 在region存在的情况下，会解析成sh-pd-fat-nacos.nepxion.com
     //    2.2 在region缺失的情况下，会解析成fat-nacos.nepxion.com
     public static final String REGION_NAME = "region";
     public static final String REGION_SEPARATE = "-";
